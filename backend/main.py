f69452d904e454da88897f768538a0e9f50d79a2
hanadyG <80218111+hanadyG@users.noreply.github.com>
2022-11-20 05:45:25 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from aws import get_data
 from pydantic import BaseModel
 # from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.cors import CORSMiddleware
+from recommender.py import recommendation
 
 class Item(BaseModel):
     img_base: str
@@ -33,6 +34,13 @@ def read_root():
 def update_item(item: Item):
     vector = get_data(item.img_base)
     return {"data": vector}
+
+@app.post("/recommendation")
+def update_item(item: Item):
+    vector = get_data(item.img_base)
+    pubinput=[['the lord of the rings: the two towers',0.2],['the lord of the rings: the two towers',0.7],['the lord of the rings: the two towers',0.1]]
+    rec=recommendation(vector,'pubinput')   
+    return {"recommendation": rec}
 
 app.add_middleware(
     CORSMiddleware,
